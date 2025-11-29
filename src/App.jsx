cef49d3a8354d525b8703874fc724871c07958d6
G S Tejas <gudur.tejasgs@gmail.com>
2025-11-29 14:02:53 +0530

    Fix import path case sensitivity for Pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
-import GalleryPage from './pages/GalleryPage';
+import GalleryPage from './Pages/GalleryPage';
 
 function App() {
   return (
